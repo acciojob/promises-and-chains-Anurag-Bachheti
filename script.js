d7f3376c20d4c4c0a0c31e5fc9f0a901ccb0c566
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-17 12:39:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+
+let button  =  document.getElementById("name");
+
+button.addEventListener("click", votePower;
+
+function votePower(){
+	let age = document.getElementById("age").value;
+	let name = Number(document.getElementById("name").value);
+
+	if(!age || !name){
+		alert("Please enter valid details.")
+	}else{
+		if(age < 18){
+			setTimeout(()=> {
+				alert(`Oh sorry ${name}. You aren't old enough.`)
+			}, 4000)
+		}else{
+			setTimeout(()=> {
+				alert(`Welcome, ${name}. You can vote.`)
+			}, 4000)
+		}
+	}
+}
